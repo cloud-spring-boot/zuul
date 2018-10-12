2b016b9bcbae6cb2aed02d9f16aaa4a63f334691
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-12 13:09:36 +0300

    Zuul service gateway initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# zuul
+# hello-service
+
+
+## Build project
+`mvn clean install`
+
+## Run locally
+`mvn spring-boot:run`
+
+## Test manually
+`curl localhost:8080/hello/maksym/stepanenko`
+
+## Build docker image
+`mvn clean package docker:build`
+
+
+## Copied from
+`https://github.com/carnellj/spmia-chapter1`
---
+++ src/main/docker/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "********************************************************"
+echo "Waiting for the Eureka to start on port $EUREKASERVER_PORT"
+echo "********************************************************"
+while ! `nc -z eureka $EUREKASERVER_PORT `; do sleep 3; done
+echo ">>>>>>>>>>>> Eureka has started"
+
+echo "********************************************************"
+echo "Starting zuul-service "
+echo "********************************************************"
+java \
+-Djava.security.egd=file:/dev/./urandom \
+-Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI \
+-jar /usr/local/zuul/@project.build.finalName@.jar
