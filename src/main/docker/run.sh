5da3d42dfae0d2ddf40baf934f07fcea03236075
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-12 14:47:00 +0300

    Config service added for Zuul.

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -7,9 +7,17 @@ while ! `nc -z eureka $EUREKASERVER_PORT `; do sleep 3; done
 echo ">>>>>>>>>>>> Eureka has started"
 
 echo "********************************************************"
+echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
+echo "********************************************************"
+while ! `nc -z config-service $CONFIGSERVER_PORT `; do sleep 3; done
+echo ">>>>>>>>>>>> Configuration Server has started"
+
+echo "********************************************************"
 echo "Starting zuul-service "
 echo "********************************************************"
 java \
 -Djava.security.egd=file:/dev/./urandom \
 -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI \
+-Dspring.cloud.config.uri=$CONFIGSERVER_URI \
+-Dspring.profiles.active=$PROFILE \
 -jar /usr/local/zuul/@project.build.finalName@.jar
--- src/main/java/com/max/zuul/ZuulApplication.java
+++ src/main/java/com/max/zuul/ZuulApplication.java
@@ -2,11 +2,13 @@ package com.max.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @SpringBootApplication
 @EnableZuulProxy
+@RefreshScope
 public class ZuulApplication {
 
     public static void main(String[] args) {
